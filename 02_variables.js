8e99790922344179e033ceb35442942661612b7f
SandipNakrani21 <142106057+SandipNakrani21@users.noreply.github.com>
2024-07-25 11:07:52 +0000

    variables and datatype done

---
+++ 02_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountName = "Sandip Patel";
+var accountEmail = "sandip@gmail.com";
+accountCity = "Surat";
+let accountState;
+
+// accountId = 2
+
+accountName = "SP"
+accountEmail = "sp@gmail.com";
+accountCity = "Ahmedabad";
+
+/*
+    Please Please Please 
+    Don't use var keyword for variable
+    because of block scope and functional scope issue.
+*/
+
+console.log([accountId,accountName,accountEmail,accountCity,accountState])
---
+++ 03_Datatype.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3);
+
+// console.log(3+3); console.log("Sandip") // Working but not good coding practice 
+
+// Working but not good coding practice 
+// console.log(3
+// + 3
+// ); 
+
+console.log(3+3); 
+
+console.log("Sandip");
+
+// Datatype List 
+/*
+    number => 2 to power 53
+    bigint => 
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined => undefined
+    symbol => unique value
+
+    // object 
+*/
+
+console.log(typeof "sandip");
+console.log(typeof 25);
+console.log(typeof 22222222222222222222222222254444444444444444444444444444444444444444444444444444444444444444433333);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
