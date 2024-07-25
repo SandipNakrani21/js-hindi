933caf11efb07c08937656ecaa8d832f52720a16
SandipNakrani21 <142106057+SandipNakrani21@users.noreply.github.com>
2024-07-25 11:37:05 +0000

    Type Conversion done

---
+++ 04_TypeConversion.js
@@ -0,0 +1,23 @@
+// let score = NaN;
+
+// console.log(score);
+// console.log(typeof(score));
+
+// let convertNumber = Number(score);
+
+// console.log(convertNumber);
+// console.log(typeof(convertNumber));
+
+
+// let IsLoggedIn = "sandip";
+// let bolIsLoggedIn = Boolean(IsLoggedIn);
+
+// console.log(bolIsLoggedIn);
+// console.log(typeof(bolIsLoggedIn));
+
+
+let SomeNumber = 33;
+let StringSomeNumber = String(SomeNumber);
+console.log(StringSomeNumber);
+console.log(typeof(StringSomeNumber));
+
